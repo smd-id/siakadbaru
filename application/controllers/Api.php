d44792d5b78fc4dee7c8d2ff6d1e0ee090ff914a
achdiadsyah <achdiadsyah@gmail.com>
2022-11-27 11:24:54 +0700

    TEst

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -12,19 +12,19 @@ class Api extends CI_Controller {
   {
     $this->load->model('M_Peserta');
     $this->load->model('M_Wawancarapsb');
+    
+    $get = $this->M_Peserta->get_all_by('s_lulus_adm', '1');
 
-
-    $data = $this->M_Peserta->get_all_by('s_lulus_adm', '1');
-    foreach ($data as $key) {
-      $load = [
-        'nik'     => $key->nik,
-        'step_1'  => '0',
-        'step_2'  => '0',
-        'step_3'  => '0',
-      ];
-
-      $this->M_Wawancarapsb->insert($load);
-    }
+    $this->M_Wawancarapsb->insert($get);
+    // foreach ($get as $key) {
+    //   $data = array(
+    //     'nik'     => $key->nik,
+    //     'step_1'  => '0',
+    //     'step_2'  => '0',
+    //     'step_3'  => '0',
+    //   );
+      
+    // }
 
   }
 
